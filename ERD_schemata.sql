39b428115a7b3a05f15aa0d7295e30972dd85d83
galvan0806 <galvan0806@gmail.com>
2024-08-19 18:32:23 -0500

    ERD

--- ERD_schemata.sql
+++ ERD_schemata.sql
@@ -1,43 +1,47 @@
+--Departements
+
 departments
 -
 dept_no PK VARCHAR 
 dept_name VARCHAR
 
+
+--Departments Employees
+dept_emp
+-
+emp_no INT FK >- employees.emp_no 
+dept_no VARCHAR FK >- departments.dept_no
+
+
+--Departments Managers
+dept_managers
+-
+dept_no VARCHAR FK >- departments.dept_no
+emp_no INT FK >- employees.emp_no
+
+
+--Employees
 employees
 -
 emp_no PK INT
+emp_title_id VARCHAR
 birth_date DATE 
 first_name VARCHAR
 last_name VARCHAR
 gender VARCHAR
 hire_date DATE 
 
-dept_emp
--
-emp_no INT FK >- employees.emp_no 
-dept_no VARCHAR FK >- departments.dept_no
-from_date DATE 
-to_date DATE 
 
-
-dept_managers
--
-dept_no VARCHAR FK >- departments.dept_no
-emp_no INT FK >- employees.emp_no
-from_date DATE 
-to_date DATE 
-
-
+--Salaries
 salaries
 -
 emp_no INT FK >- employees.emp_no
 salary BIGINT
-from_date DATE 
-to_date DATE 
 
+
+--Titles
 titles
 -
 emp_no INT FK >- employees.emp_no
 title VARCHAR
-from_date DATE 
-to_date DATE 
+ 
