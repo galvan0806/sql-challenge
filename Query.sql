28b74cb541b734a319318c48340af79916f32c2e
galvan0806 <galvan0806@gmail.com>
2024-08-21 19:21:03 -0500

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -62,7 +62,7 @@ SELECT
 	employees.first_name, 
 	employees.sex, 
 	salaries.salary
-FROM employees
+FROM employees 
 	LEFT JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 -- 2.List the first name, last name, and hire date for the employees who were hired in 1986.
@@ -97,15 +97,15 @@ FROM employees
 -- 5.List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 SELECT 
 	first_name, 
-	last_name, sex
+	last_name, 
+	sex
 FROM employees
 WHERE first_name = 'Hercules' 
 	AND last_name LIKE 'B%';
 
 -- 6. List each employee in the Sales department, including their employee number, last name, and first name.
 SELECT * FROM departments;
-
--- The department number of Sales department is d007
+--The department number of Sales department = d007
 SELECT 
 	employees.emp_no, 
 	employees.last_name, 
@@ -118,7 +118,7 @@ WHERE departments.dept_no = 'd007';
 
 -- 7.List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT * FROM departments;
--- The department numbers of Sales and Development departments are d007 and d005
+-- The department numbers of Sales and Development departments = d007 and d005
 SELECT 
 	employees.emp_no, 
 	employees.last_name, 
@@ -128,7 +128,6 @@ FROM employees
 	LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 	LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
-
 
 -- 8.List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT 
